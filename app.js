370b4012f06df11abd96d597f2f4d3514ffd3e91
alvarogarcia1010 <00111317@uca.edu.sv>
2019-06-25 23:36:23 -0600

    Cargando todos los reportes

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var firebase = require('firebase-admin') // Trabajar con la base de datos
-var serviceAccount = require('./firebase-config.json')
 
 var indexRouter = require('./routes/index');
 // var apiRouter = require('./routes/api');
 
 var app = express();
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: "https://ureport-93a7b.firebaseio.com"
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,12 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-var database = firebase.database();
-var reports = database.ref('/reports')
-reports.once("value", function(snapshot) {
-  console.log("El dato es", snapshot.val());
-});
 
 app.use('/', indexRouter);
 //app.use('/api/Buscadoresweb', apiRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require('firebase-admin') // Trabajar con la base de datos
+var serviceAccount = require('../firebase-config.json')
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://ureport-93a7b.firebaseio.com"
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var reportsList;
+  const reports = firebase.database().ref('/reports')
+  reports.on('value', (snapshot) => {
+    reportsList = snapshot.val();
+    res.render('index', { reports: reportsList });
+  })
 });
 
 module.exports = router;
